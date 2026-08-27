d79140f55fdc3518e06575a7751867f05dc09ee1
Nexus <139140443+Nexusbusiness-ofc@users.noreply.github.com>
2026-08-27 15:59:09 +0100

    Make music player background ultra-translucent with HDR mesh in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MusicFlow Service Worker - Network-First Strategy for Instant Updates
-const CACHE_NAME = 'musicflow-live-v12';
+const CACHE_NAME = 'musicflow-live-v13';
 
 self.addEventListener('install', (e) => {
   // Activate immediately without waiting for tabs to close
