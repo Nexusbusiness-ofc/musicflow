446d3a6f401013fb0b480cb20bac20065da4be73
Nexus <139140443+Nexusbusiness-ofc@users.noreply.github.com>
2026-08-28 13:17:39 +0100

    Add new stunning MusicFlow app icon and PWA assets in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // MusicFlow Service Worker - Network-First Strategy for Instant Updates
-const CACHE_NAME = 'musicflow-live-v28';
+const CACHE_NAME = 'musicflow-live-v29';
 
 self.addEventListener('install', (e) => {
   // Activate immediately without waiting for tabs to close
