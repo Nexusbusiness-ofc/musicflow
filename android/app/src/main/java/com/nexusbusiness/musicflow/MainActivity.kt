5e360958ac62e5545549d011ef21638513f95ba5
Nexus <139140443+Nexusbusiness-ofc@users.noreply.github.com>
2026-08-28 11:44:57 +0100

    Configure Google Mobile Ads AdMob SDK & Android native project with App ID and Ad Unit in android/app/src/main/java/com/nexusbusiness/musicflow/MainActivity.kt

---
+++ android/app/src/main/java/com/nexusbusiness/musicflow/MainActivity.kt
@@ -0,0 +1,74 @@
+﻿package com.nexusbusiness.musicflow
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var adView: AdView
+
+    // Google AdMob Configuration
+    private val ADMOB_APP_ID = "ca-app-pub-2898806335291249~9336449403"
+    private val BANNER_AD_UNIT_ID = "ca-app-pub-2898806335291249/7684757702"
+    private val APP_URL = "https://nexusbusiness-ofc.github.io/musicflow/"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 1. Initialize Google Mobile Ads SDK
+        MobileAds.initialize(this) { initializationStatus ->
+            // SDK initialized, load Banner Ad
+            loadBannerAd()
+        }
+
+        // 2. Setup WebView for MusicFlow
+        webView = findViewById(R.id.webView)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+        webSettings.allowFileAccess = true
+        webSettings.mediaPlaybackRequiresUserGesture = false
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl(APP_URL)
+    }
+
+    private fun loadBannerAd() {
+        adView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        if (::adView.isInitialized) {
+            adView.destroy()
+        }
+        super.onDestroy()
+    }
+}
